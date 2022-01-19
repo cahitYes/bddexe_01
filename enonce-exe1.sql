193ca765ac5e5d1fc08ff9593781c3365758c8a3
mikhawa <michaeljpitz@gmail.com>
2022-01-19 11:22:46 +0100

    enonce

--- enonce-exe1.sql
+++ enonce-exe1.sql
@@ -1,8 +1,4 @@
 -- Sélectionnez tous les champs de `categ` ordonnés par `name` ascendant
-SELECT * FROM `categ` ORDER BY `name` ASC;
-
 -- Séléctionnez `idcateg` et `name` de `categ` quand `idcateg` vaut 4
-SELECT `idcateg`, `name` FROM `categ` WHERE `idcateg` = 4;
-
 -- Séléctionnez `idcateg` et `name` de `categ` quand `idcateg` se trouve entre 2 et 4
-SELECT `idcateg`, `name` FROM `categ` WHERE `idcateg` BETWEEN 2 AND 4;
+-- Séléctionnez `idcateg` et `name` de `categ` quand `idcateg` se trouve est 1, 3 ou 5 ordonné par `name` descendant
