b588cdd00a5b3d110efc1c046a2f3b9f6ef11a70
mikhawa <michaeljpitz@gmail.com>
2022-01-19 11:10:41 +0100

    BETWEEN

--- exe1.sql
+++ exe1.sql
@@ -1,4 +1,8 @@
 -- Sélectionnez tous les champs de `categ` ordonnés par `name` ascendant
 SELECT * FROM `categ` ORDER BY `name` ASC;
 
+-- Séléctionnez `idcateg` et `name` de `categ` quand `idcateg` vaut 4
 SELECT `idcateg`, `name` FROM `categ` WHERE `idcateg` = 4;
+
+-- Séléctionnez `idcateg` et `name` de `categ` quand `idcateg` se trouve entre 2 et 4
+SELECT `idcateg`, `name` FROM `categ` WHERE `idcateg` BETWEEN 2 AND 4;
